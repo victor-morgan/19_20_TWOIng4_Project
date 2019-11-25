0af79e322d5542405268e8899404294535f112c7
victor valée <valee.victorj@gmail.com>
2019-11-25 18:54:53 +0100

    init

---
+++ frontend/src/Components/Barre.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Barre extends Component {
+    render() {
+        return (
+            <div>
+                dcjvksnjdk
+            </div>
+        );
+    }
+}
+
+export default Barre;
