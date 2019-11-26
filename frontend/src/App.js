e2210d1b26f77fd2543b520b3b14a0ccd7df993e
victor valée <valee.victorj@gmail.com>
2019-11-26 11:51:33 +0100

    responsive positionnement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component{
   render(){
   return (
     <div className="App">
-      <p>zefczfcziek</p>
+      
       <Barre/>
       <Widget/>
     </div>
--- frontend/src/Components/Widget.js
+++ frontend/src/Components/Widget.js
@@ -7,9 +7,19 @@ class Widget extends Component {
                 <div class="row">
                     <div class="col-lg-2"></div>
                     <div class="col-lg-10">
-                        <div class="row">
-                        <section id="albums" class="row">
-                        <article class="col-xl-2 col-md-3 col-sm-4 col-6">
+                    <section class="row">
+                    <article class="col-lg-12 col-md-9 col-xs-3">
+                    <div>DASHBOARD</div>
+                   <br>
+                   </br>
+                   <br>
+                   </br>
+                        </article>
+                        </section>
+
+
+                        <section class="row">
+                        <article class="col-lg-4 col-md-3 col-xs-2">
 						<figure>
 							
 							<figcaption>Native (Deluxe)</figcaption>
@@ -17,7 +27,7 @@ class Widget extends Component {
 						<div class="artist">OneRepublic</div>
 						<div class="count">17 songs</div>
 					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
+					<article class="col-lg-4 col-md-3 col-xs-2">
 						<figure>
 							
 							<figcaption>Wax Wings</figcaption>
@@ -25,7 +35,7 @@ class Widget extends Component {
 						<div class="artist">Joshua Radin</div>
 						<div class="count">11 songs</div>
 					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
+					<article class="col-lg-4 col-md-3 col-xs-2">
 						<figure>
 							
 							<figcaption>Modern Vampires of th...</figcaption>
@@ -33,7 +43,15 @@ class Widget extends Component {
 						<div class="artist">Vampire Weekend</div>
 						<div class="count">12 songs</div>
 					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
+                    </section>
+
+                    <br>
+                    </br>
+                    <br>
+                    </br>
+
+                    <section class="row">
+					<article class="col-lg-4 col-md-3 col-xs-2">
 						<figure>
 							
 							<figcaption>Night Visions</figcaption>
@@ -41,23 +59,8 @@ class Widget extends Component {
 						<div class="artist">Imagine Dragons</div>
 						<div class="count">11 songs</div>
 					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
-						<figure>
-							
-							<figcaption>Trouble Will Find Me</figcaption>
-						</figure>
-						<div class="artist">The National</div>
-						<div class="count">13 songs <span class="explicit">EXPLICIT</span></div>
-					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
-						<figure>
-							
-							<figcaption>Native (Deluxe)</figcaption>
-						</figure>
-						<div class="artist">OneRepublic</div>
-						<div class="count">17 songs</div>
-					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
+				
+					<article class="col-lg-4 col-md-3 col-xs-2">
 						<figure>
 							
 							<figcaption>Wax Wings</figcaption>
@@ -65,7 +68,7 @@ class Widget extends Component {
 						<div class="artist">Joshua Radin</div>
 						<div class="count">11 songs</div>
 					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
+					<article class="col-lg-4 col-md-3 col-xs-2">
 						<figure>
 							
 							<figcaption>Modern Vampires of th...</figcaption>
@@ -73,24 +76,9 @@ class Widget extends Component {
 						<div class="artist">Vampire Weekend</div>
 						<div class="count">12 songs</div>
 					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
-						<figure>
-							
-							<figcaption>Night Visions</figcaption>
-						</figure>
-						<div class="artist">Imagine Dragons</div>
-						<div class="count">11 songs <span class="explicit">EXPLICIT</span></div>
-					</article>
-					<article class="col-xl-2 col-md-3 col-sm-4 col-6">
-						<figure>
-							
-							<figcaption>Trouble Will Find Me</figcaption>
-						</figure>
-						<div class="artist">The National</div>
-						<div class="count">13 songs</div>
-					</article>
+				
 </section>
-                        </div>
+                        
                     </div>
                 </div>
              </div>
